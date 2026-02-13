490a388df2401773ee53897105f74b21ff3127ce
MhZ9191 <hegic91@gmail.com>
2026-02-13 15:59:14 +0100

    Added API

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,5 +2,17 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 export default function Main() {
+  const urlAPI = "https://lanciweb.github.io/demo/api/actresses/";
+
+  const [getData, setGetData] = useState([]);
+
+  const fetchData = () => {
+    axios.get(urlAPI).then((res) => {
+      setGetData(res.data);
+    });
+  };
+
+  useEffect(fetchData, []);
+
   return <div></div>;
 }
