58407d966d396817949dfe3ac2f943b23b40183c
MhZ9191 <hegic91@gmail.com>
2026-02-13 15:54:32 +0100

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header({ title }) {
+  return (
+    <header>
+      <section>
+        <div>
+          <h1>{title}</h1>
+        </div>
+      </section>
+    </header>
+  );
+}
